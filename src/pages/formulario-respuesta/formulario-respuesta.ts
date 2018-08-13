2b0390f48eebfb250052c138f20b39ef60d5ab7e
Elvis Alcántara Pinedo <diskcom123@gmail.com>
2018-08-13 12:59:15 -0500

    textarea disabled
    update

--- src/pages/formulario-respuesta/formulario-respuesta.ts
+++ src/pages/formulario-respuesta/formulario-respuesta.ts
@@ -7,11 +7,12 @@ import { CursoService } from '../../providers/curso/curso'
   templateUrl: 'formulario-respuesta.html',
 })
 export class FormularioRespuestaPage {
-
+	descripcion: string = "";
   constructor(public navCtrl: NavController, public navParams: NavParams, private _cs: CursoService) {
   	console.log(this.navParams.get("curso"),this.navParams.get("etapa"),this.navParams.get("formulario"),this.navParams.get("url"));
   }
   ionViewWillEnter(){
     this._cs.getRespuesta(this.navParams.get("curso"),this.navParams.get("etapa"),this.navParams.get("formulario"),this.navParams.get("url"));
   }
+
 }
