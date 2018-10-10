1e428715a124e4c2d234168adcce6171b3f20cde
Elvis Alcántara Pinedo <diskcom123@gmail.com>
2018-10-10 13:43:23 -0500

    update
    update

--- src/pages/teacher-usuario/teacher-usuario.ts
+++ src/pages/teacher-usuario/teacher-usuario.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AlumnoService } from '../../providers/alumno/alumno';
-import { UsuarioPage } from '../index.paginas';
 
 @Component({
   selector: 'page-teacher-usuario',
